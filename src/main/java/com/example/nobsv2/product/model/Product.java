8efe77afd63347edfd7001fbe19061de01de3c8f
GabeMike52 <gmiqueiascarneiro52@gmail.com>
2024-12-14 13:34:11 -0300

    Now it is working, had to drop lombok and manually set the Getters and Setters.

---
+++ src/main/java/com/example/nobsv2/product/model/Product.java
@@ -0,0 +1,56 @@
+package com.example.nobsv2.product.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "product")
+//@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Product {
+
+    @Id //Primary Key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //Auto generates the id
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    //@JsonProperty("name")
+    private String name;
+
+    @Column(name = "description")
+    //@JsonProperty("description")
+    private String description;
+
+    @Column(name = "price")
+    //@JsonProperty("price")
+    private Double price;
+
+
+    //Getters and Setters
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
